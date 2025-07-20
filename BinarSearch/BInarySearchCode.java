4ebee8aae2d0bdc176d47550af8ec4be0787db23
Ayesha qureshi <qureshiayesha660@gmail.com>
2025-07-20 16:15:39 +0530

    bs

--- BinarSearch/BInarySearchCode.java
+++ BinarSearch/BInarySearchCode.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class BInarySearchCode {
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,9,11,12,14,20,36,48};
+        int target = 36;
+        int ans = BS(arr,target);
+        System.out.println(ans);
+
+    }
+    static int BS(int arr[],int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end) {
+            //    int mid = (start+end)/2 -> now bcz of this the error can occur in which might be possible that (start+end) exeeds the range of integer in java
+            //better way to find the mid!
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+            return -1;
+
+    }
 }
--- BinarSearch/OrderAgnosticBinarySear.java
+++ BinarSearch/OrderAgnosticBinarySear.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class OrderAgnosticBinarySear {
+    public static void main(String[] args) {
+        int arr[] = {3,4,5,6,7,8,9};
+        int target = 5;
+        int ans = OABS(arr,target);
+        System.out.println(ans);
+
+    }
+    static int OABS(int arr[],int target){
+        int start = 0;
+        int end = arr.length-1;
+
+       boolean isAsc = arr[start]<arr[end];
+
+            while(start<=end) {
+                //    int mid = (start+end)/2 -> now bcz of this the error can occur in which might be possible that (start+end) exeeds the range of integer in java
+                //better way to find the mid!
+                int mid = start + (end - start) / 2;
+
+                if (arr[mid] == target) {
+                    return mid;
+                }
+                if (isAsc) {
+                    if (target < arr[mid]) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
+                    }
+                } else {
+                    if (target > arr[mid]) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
+                    }
+                }
+            }
+        return -1;
+    }
 }
