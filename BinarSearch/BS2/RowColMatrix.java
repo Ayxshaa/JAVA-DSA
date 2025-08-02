d4fdc450bbb53bd1bf86383e9d5d9abed1240858
Ayesha qureshi <qureshiayesha660@gmail.com>
2025-08-02 10:38:54 +0530

    array

--- BinarSearch/BS2/RowColMatrix.java
+++ BinarSearch/BS2/RowColMatrix.java
@@ -1,2 +1,34 @@
+import java.util.Arrays;
+
 public class RowColMatrix {
+    public static void main(String[] args) {
+        int arr[][] = {
+                {10,20,30,40},
+                {11,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+        };
+
+        System.out.println(Arrays.toString(search(arr,50)));
+
+    }
+
+    static int[] search(int arr[][], int target){
+               int r =0;
+               int c = arr.length-1;
+
+               while (r<arr.length && c>=0){
+                   if(arr[r][c] == target){
+                       return new int[]{r,c};
+                   }
+                   if(arr[r][c]<target){
+                       r++;
+                   }
+                   else{
+                       c--;
+                   }
+               }
+               return new int[]{-1,-1};
+    }
+
 }
--- BinarSearch/BS2/transponse.java
+++ BinarSearch/BS2/transponse.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class transponse {
+    public static void main(String[] args) {
+          int arr[][] = {
+                  {2,4,-1},
+                  {-10,5,11},
+                  {18,-7,6}
+          };
+          int ans[][] = change(arr);
+        System.out.println(Arrays.deepToString(ans));
+    }
+
+    static int[][] change(int matrix[][]){
+        int n = matrix.length;
+        for(int k =0; k<n; k++){
+            int r =1;
+            for(int c =1; c<n; c++,r++ ){
+                int temp = matrix[k][c];
+                matrix[k][c] = matrix[r][k];
+                matrix[r][k] = temp;
+            }
+        }
+        return matrix;
+    }
 }
