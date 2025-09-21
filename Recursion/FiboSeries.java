b7055a07cc4fd97698e764228483cb4d98c60ee7
Ayesha qureshi <qureshiayesha660@gmail.com>
2025-09-21 20:49:37 +0530

    time complexity-> fiboformula

--- Recursion/FiboSeries.java
+++ Recursion/FiboSeries.java
@@ -1,6 +1,14 @@
 public class FiboSeries {
     public static void main(String[] args) {
-        System.out.println(fibo(6));
+        // System.out.println(fibo(6));
+        for(int i=0; i<10; i++){
+            System.out.print(fiboFormula(i)+" ");
+        }
+    }
+    static int fiboFormula(int n ){
+        //   return (int)Math.pow(((1+Math.sqrt(5))/2), n) - (int)Math.pow(((1-Math.sqrt(5))/2), n) / Math.sqrt(5);
+        return (int)Math.pow(((1+Math.sqrt(5))/2), n) / Math.sqrt(5);
+    
     }
     static int fibo(int n ){
         // base condition
