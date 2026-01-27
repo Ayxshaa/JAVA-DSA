b0665afcff5d3566c638e0969f15673ed4d7d97b
Ayesha <qureshiayesha660@gmail.com>
2026-01-27 12:28:34 +0530

    starting of oops

---
+++ OOP/1stSection/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Student students[] = new Student[5];
+
+
+        // this is just declaring, for now its null!
+        Student ayesha;
+    }
+
+}
+class Student{
+    int rno[] = new int[5];
+    String names[] = new String[5];
+    float marks[] = new float[5];
+}
